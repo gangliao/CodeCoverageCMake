11a5b14e360b99b4abee1ace82dd164a4cdf652c
liaogang <liaogang@baidu.com>
2016-10-31 15:50:32 +0800

    Revise Cmake

--- cmake/Coveralls.cmake
+++ cmake/Coveralls.cmake
@@ -1,61 +1,71 @@
-#
-# Param _COVERAGE_SRCS	A list of source files that coverage should be collected for.
-# Param _COVERALLS_UPLOAD Upload the result to coveralls?
-#
+# Code Coverage CMake Script. If _COVERALLS_UPLOAD is ON, it will upload gcov json
+# data to overalls.io automatically. 
 
-function(coveralls_setup _COVERAGE_SRCS _COVERALLS_UPLOAD _CMAKE_SCRIPT_PATH)
-	# When passing a CMake list to an external process, the list
-	# will be converted from the format "1;2;3" to "1 2 3".
-	# http://cmake.3232098.n2.nabble.com/Passing-a-CMake-list-quot-as-is-quot-to-a-custom-target-td6505681.html
-	set(COVERAGE_SRCS_TMP ${_COVERAGE_SRCS})
-	set(COVERAGE_SRCS "")
-	foreach (COVERAGE_SRC ${COVERAGE_SRCS_TMP})
-		set(COVERAGE_SRCS "${COVERAGE_SRCS}*${COVERAGE_SRC}")
-	endforeach()
-
-	set(COVERALLS_FILE ${PROJECT_BINARY_DIR}/coveralls.json)
+# Param _COVERAGE_SRCS	        A list of coverage source files.
+# Param _COVERALLS_UPLOAD       Upload the result to coveralls.
+# Param _CMAKE_SCRIPT_PATH      CMake script path.
+function(code_coverage _COVERAGE_SRCS _COVERALLS_UPLOAD _CMAKE_SCRIPT_PATH)
+    # clean previous gcov data.
     file(REMOVE_RECURSE ${PROJECT_BINARY_DIR}/*.gcda)
 
-	add_custom_target(coveralls_generate
+    # find curl for upload JSON soon.
+    if (_COVERALLS_UPLOAD)
+		find_program(CURL_EXECUTABLE curl)
+		if (NOT CURL_EXECUTABLE)
+			message(FATAL_ERROR "Coveralls: curl not found!")
+		endif()
+    endif()
+
+	# When passing a CMake list to an external process, the list
+	# will be converted from the format "1;2;3" to "1 2 3".
+	set(COVERAGE_SRCS "")
+	foreach (SINGLE_SRC ${_COVERAGE_SRCS})
+		set(COVERAGE_SRCS "${COVERAGE_SRCS}*${SINGLE_SRC}")
+	endforeach()
+
+    # coveralls json file.
+	set(COVERALLS_FILE ${PROJECT_BINARY_DIR}/coveralls.json)
+
+ 	add_custom_target(coveralls_generate
 		# Run regress tests.
 		COMMAND ${CMAKE_CTEST_COMMAND} --output-on-failure
 		# Generate Gcov and translate it into coveralls JSON.
-		# We do this by executing an external CMake script.
-		# (We don't want this to run at CMake generation time, but after compilation and everything has run).
 		COMMAND ${CMAKE_COMMAND}
-				-DCOVERAGE_SRCS="${COVERAGE_SRCS}" # TODO: This is passed like: "a b c", not "a;b;c"
+				-DCOVERAGE_SRCS="${COVERAGE_SRCS}"
 				-DCOVERALLS_OUTPUT_FILE="${COVERALLS_FILE}"
 				-DCOV_PATH="${PROJECT_BINARY_DIR}"
 				-DPROJECT_ROOT="${PROJECT_SOURCE_DIR}"
 				-P "${_CMAKE_SCRIPT_PATH}/CoverallsGenerateGcov.cmake"
 		WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
-		COMMENT "Generating coveralls output..."
+		COMMENT "Coveralls: generating coveralls output..."
 		)
 
 	if (_COVERALLS_UPLOAD)
 		message("COVERALLS UPLOAD: ON")
-
-		find_program(CURL_EXECUTABLE curl)
-
-		if (NOT CURL_EXECUTABLE)
-			message(FATAL_ERROR "Coveralls: curl not found! Aborting")
-		endif()
-
+        # Upload the JSON to coveralls.
 		add_custom_target(coveralls_upload
-			# Upload the JSON to coveralls.
 			COMMAND ${CURL_EXECUTABLE}
 					-S -F json_file=@${COVERALLS_FILE}
 					https://coveralls.io/api/v1/jobs
-
 			DEPENDS coveralls_generate
-
 			WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
-			COMMENT "Uploading coveralls output...")
+			COMMENT "Coveralls: uploading coveralls output...")
 
 		add_custom_target(coveralls DEPENDS coveralls_upload)
 	else()
 		message("COVERALLS UPLOAD: OFF")
 		add_custom_target(coveralls DEPENDS coveralls_generate)
 	endif()
-
 endfunction()
+
+set(CMAKE_BUILD_TYPE "Debug")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g -O0 -fprofile-arcs -ftest-coverage")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -g -O0 -fprofile-arcs -ftest-coverage")
+
+set(LIB_SRC ${PROJECT_SOURCE_DIR}/src/base.c)
+
+code_coverage(
+    "${LIB_SRC}" 
+    ${COVERALLS_UPLOAD}                 
+    "${PROJECT_SOURCE_DIR}/cmake"
+)
