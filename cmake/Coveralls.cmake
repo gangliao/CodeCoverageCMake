fc47855250ee480a37aff77948c645ba0a7b98de
liaogang <liaogang@baidu.com>
2016-10-31 16:02:02 +0800

    revise indent

--- cmake/Coveralls.cmake
+++ cmake/Coveralls.cmake
@@ -10,51 +10,51 @@ function(code_coverage _COVERAGE_SRCS _COVERALLS_UPLOAD _CMAKE_SCRIPT_PATH)
 
     # find curl for upload JSON soon.
     if (_COVERALLS_UPLOAD)
-		find_program(CURL_EXECUTABLE curl)
-		if (NOT CURL_EXECUTABLE)
-			message(FATAL_ERROR "Coveralls: curl not found!")
-		endif()
+        find_program(CURL_EXECUTABLE curl)
+        if (NOT CURL_EXECUTABLE)
+            message(FATAL_ERROR "Coveralls: curl not found!")
+        endif()
     endif()
 
 	# When passing a CMake list to an external process, the list
 	# will be converted from the format "1;2;3" to "1 2 3".
 	set(COVERAGE_SRCS "")
 	foreach (SINGLE_SRC ${_COVERAGE_SRCS})
-		set(COVERAGE_SRCS "${COVERAGE_SRCS}*${SINGLE_SRC}")
+        set(COVERAGE_SRCS "${COVERAGE_SRCS}*${SINGLE_SRC}")
 	endforeach()
 
     # coveralls json file.
 	set(COVERALLS_FILE ${PROJECT_BINARY_DIR}/coveralls.json)
 
  	add_custom_target(coveralls_generate
-		# Run regress tests.
-		COMMAND ${CMAKE_CTEST_COMMAND} --output-on-failure
-		# Generate Gcov and translate it into coveralls JSON.
-		COMMAND ${CMAKE_COMMAND}
-				-DCOVERAGE_SRCS="${COVERAGE_SRCS}"
-				-DCOVERALLS_OUTPUT_FILE="${COVERALLS_FILE}"
-				-DCOV_PATH="${PROJECT_BINARY_DIR}"
-				-DPROJECT_ROOT="${PROJECT_SOURCE_DIR}"
-				-P "${_CMAKE_SCRIPT_PATH}/CoverallsGenerateGcov.cmake"
-		WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
-		COMMENT "Coveralls: generating coveralls output..."
-		)
+        # Run regress tests.
+        COMMAND ${CMAKE_CTEST_COMMAND} --output-on-failure
+        # Generate Gcov and translate it into coveralls JSON.
+        COMMAND ${CMAKE_COMMAND}
+                -DCOVERAGE_SRCS="${COVERAGE_SRCS}"
+                -DCOVERALLS_OUTPUT_FILE="${COVERALLS_FILE}"
+                -DCOV_PATH="${PROJECT_BINARY_DIR}"
+                -DPROJECT_ROOT="${PROJECT_SOURCE_DIR}"
+                -P "${_CMAKE_SCRIPT_PATH}/CoverallsGenerateGcov.cmake"
+        WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
+        COMMENT "Coveralls: generating coveralls output..."
+        )
 
 	if (_COVERALLS_UPLOAD)
-		message("COVERALLS UPLOAD: ON")
+        message("COVERALLS UPLOAD: ON")
         # Upload the JSON to coveralls.
-		add_custom_target(coveralls_upload
-			COMMAND ${CURL_EXECUTABLE}
-					-S -F json_file=@${COVERALLS_FILE}
-					https://coveralls.io/api/v1/jobs
-			DEPENDS coveralls_generate
-			WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
-			COMMENT "Coveralls: uploading coveralls output...")
+        add_custom_target(coveralls_upload
+            COMMAND ${CURL_EXECUTABLE}
+                    -S -F json_file=@${COVERALLS_FILE}
+                    https://coveralls.io/api/v1/jobs
+            DEPENDS coveralls_generate
+            WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
+            COMMENT "Coveralls: uploading coveralls output...")
 
-		add_custom_target(coveralls DEPENDS coveralls_upload)
+        add_custom_target(coveralls DEPENDS coveralls_upload)
 	else()
-		message("COVERALLS UPLOAD: OFF")
-		add_custom_target(coveralls DEPENDS coveralls_generate)
+        message("COVERALLS UPLOAD: OFF")
+        add_custom_target(coveralls DEPENDS coveralls_generate)
 	endif()
 endfunction()
 
